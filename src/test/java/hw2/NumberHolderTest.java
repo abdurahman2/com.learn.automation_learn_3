219fe61b13e56751740104c3d97f6bacc2973f7f
Dmytro Vasko <Dmytro_Vasko@epam.com>
2022-07-09 00:00:19 +0300

    HW 2, task 4 - test was split

--- src/test/java/hw2/NumberHolderTest.java
+++ src/test/java/hw2/NumberHolderTest.java
@@ -12,13 +12,18 @@ public class NumberHolderTest {
     NumberHolder numberHolder2 = new NumberHolder();
 
     @Test
-    @DisplayName("kjnh")
+    @DisplayName("Data from constructor")
     public void checkData() {
+        assertEquals(7,numberHolder.getAnInt(),"Data are not equal to the AnInt parameter");
+        assertEquals(3.58F,numberHolder.getaFloat(),"Data are not equal to the aFloat parameter");
+    }
+
+    @Test
+    @DisplayName("Data was set")
+    public void checkData2(){
         numberHolder2.setAnInt(7);
         numberHolder2.setaFloat(3.58F);
-        assertEquals(7,numberHolder.getAnInt(),"Data are not equal to the AnInt parameter");
         assertEquals(7,numberHolder2.getAnInt(),"Data are not equal to the set AnInt parameter");
-        assertEquals(3.58F,numberHolder.getaFloat(),"Data are not equal to the aFloat parameter");
         assertEquals(3.58F,numberHolder2.getaFloat(),"Data are not equal to the set aFloat parameter");
     }
 }
