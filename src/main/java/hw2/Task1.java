bd7ee6075efe9f2f10c8c1a9b8d22b3f7f40bdbc
Dmytro Vasko <Dmytro_Vasko@epam.com>
2022-07-18 18:08:01 +0300

    HW 2, Task 1 concat

--- src/main/java/hw2/Task1.java
+++ src/main/java/hw2/Task1.java
@@ -10,15 +10,8 @@ The concatenated string with replacements: H Exercises and ython Exercises
 
  */
 public class Task1 {
-    String str1 = "PHP Exercises and ";
-    String str2 = "Python Exercises";
 
-    //    static public String concatenate(String str1, String str2) {
-//        return concatenate(str1,str2).replace("P", "").replace("p", "");
-//    }
-    static public String concatenate() {
-        String str1 = "PHP Exercises and ";
-        String str2 = "Python Exercises";
+    static public String concatenate(String str1, String str2) {
         return str1.concat(str2).replace("P", "").replace("p", "");
     }
 }
