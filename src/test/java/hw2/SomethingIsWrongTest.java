bed3dc13f8a8d38995640a8fe4a4f5fa19f3b213
Dmytro Vasko <Dmytro_Vasko@epam.com>
2022-07-19 15:32:48 +0300

    HW 2, all task done

--- src/test/java/hw2/SomethingIsWrongTest.java
+++ src/test/java/hw2/SomethingIsWrongTest.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SomethingIsWrongTest {
-    Rectangle myRect = new Rectangle(40, 50);
 
     @Test
     public void testArea() {
+        Rectangle myRect = new Rectangle(40, 50);
         assertEquals(2000, myRect.getArea(), "Mistakes in calculation of area");
     }
 }
