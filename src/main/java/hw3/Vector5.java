3cca402d4081bbd69b2df6df52313d07051a9b1b
Dmytro Vasko <Dmytro_Vasko@epam.com>
2022-07-20 10:45:00 +0300

    Changed Vector5

--- src/main/java/hw3/Vector5.java
+++ src/main/java/hw3/Vector5.java
@@ -8,9 +8,9 @@ public class Vector5 {
     private final double x;
     private final double y;
     private final double z;
-    private static final int countList = 2;
+    private static final int COUNT_LIST = 2;
 
-    Vector5(double x, double y, double z) { /* this что это и почему он нужен*/
+    Vector5(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
@@ -20,8 +20,8 @@ public class Vector5 {
         return (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)));
     }
 
-    static double scalarMulti(Vector5 a, Vector5 b) {
-        return (a.x * b.x + a.y * b.y + a.z * b.z);
+    static double scalarMulti(Vector5 vector1, Vector5 vector2) {
+        return (vector1.x * vector2.x + vector1.y * vector2.y + vector1.z * vector2.z);
     }
 
     public static Vector5 createRandomVector() {
@@ -34,19 +34,18 @@ public class Vector5 {
 
     public static List<Vector5> listRandomVector() {
         List<Vector5> vectorList = new ArrayList<>();
-        for (int i = 0; i < countList; i++) {
+        for (int i = 0; i < COUNT_LIST; i++) {
             vectorList.add(Vector5.createRandomVector());
         }
         return vectorList;
     }
 
     public static List<Vector5> listRandomVector(int size) {
-        Random random = new Random();
         List<Vector5> vectorList = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             vectorList.add(createRandomVector());
         }
-        return vectorList;
+        return listRandomVector(COUNT_LIST);
     }
 
     @Override
