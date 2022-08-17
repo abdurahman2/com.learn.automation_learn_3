eeced0d475f960378a9565dacaadd93133933589
Dmytro Vasko <Dmytro_Vasko@epam.com>
2022-08-17 19:14:19 +0300

    Lambda Stream  1 + 2 + 3 tasks + changes + tests

---
+++ src/main/java/lambdastreams/task4/StringFunction.java
@@ -0,0 +1,5 @@
+//package lambdastreams.task4;
+//
+//public interface StringFunction {
+//    String run(String str);
+//}
---
+++ src/test/java/task3/PersonalDataTest.java
@@ -0,0 +1,33 @@
+package task3;
+
+import lambdastreams.task3.Person;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class PersonalDataTest {
+
+    @Test
+    void adultPersons() {
+        List<String> right = Arrays.asList("PETER", "PAUL", "ARNOLD", "AARON");
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("Peter", "Patterson", 21));
+        personList.add(new Person("Paul", "Walker", 31));
+        personList.add(new Person("Steve", "Runner", 17));
+        personList.add(new Person("Arnold", "", -1));
+        personList.add(new Person(" ", "Stevenson", 19));
+        personList.add(new Person("   ", "Stevenson", 19));
+        personList.add(new Person("      ", "Stevenson", 19));
+        personList.add(new Person(" Arnold", "Stevenson", 19));
+        personList.add(null);
+        personList.add(new Person("Aaron", "Bernice", 18));
+
+        System.out.println(Person.createListOfUniqueNamesForAdults(personList));
+        Assertions.assertEquals(right, Person.createListOfUniqueNamesForAdults(personList),
+                "The list with adults does not match");
+    }
+}
